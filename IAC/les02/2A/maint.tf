fd093fdffe7ff839445ea8f39e8479e1c065ce52
Ruben <s1190828@student.windesheim.nl>
2025-05-17 17:42:05 +0000

    0.1

---
+++ IAC/les02/2A/maint.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    esxi = {
+      source = "josenk/esxi"
+    }
+  }
+}
+
+provider "esxi" {
+  esxi_hostname = var.esxi_hostname
+  esxi_hostport = var.esxi_hostport
+  esxi_hostssl  = var.esxi_hostssl
+  esxi_username = var.esxi_username
+  esxi_password = var.esxi_password
+}
+
+#Web servers
+resource "esxi_guest" "webserver" {
+  count        = 2
+  guest_name   = "webserver-${count.index + 1}"
+  disk_store   = "DS01"
+  ovf_source   = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.ova"
+  memsize      = 2048
+  numvcpus     = 1
+  power        = "on"
+
+  network_interfaces {
+    virtual_network = "VM Network"
+  }
+}
+
+#DB server
+resource "esxi_guest" "databaseserver" {
+  guest_name   = "databaseserver"
+  disk_store   = "DS01"
+  ovf_source   = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.ova"
+  memsize      = 2048
+  numvcpus     = 1
+  power        = "on"
+
+  network_interfaces {
+    virtual_network = "VM Network"
+  }
+}
---
+++ IAC/les02/2A/variables.tf
@@ -0,0 +1,33 @@
+#
+#  See https://www.terraform.io/intro/getting-started/variables.html for more details.
+#
+
+#  Change these defaults to fit your needs!
+
+variable "esxi_hostname" {
+  default = "ESX01.baas"
+}
+
+variable "esxi_hostport" {
+  default = "22"
+}
+
+variable "esxi_hostssl" {
+  default = "443"
+}
+
+variable "esxi_username" {
+  default = "root"
+}
+
+variable "esxi_password" { # Unspecified will prompt 
+}
+
+variable "vmIP" {
+  default = "192.168.20.14/24"
+}
+
+variable "vmGateway" {
+  default = "192.168.20.1"
+}
+
