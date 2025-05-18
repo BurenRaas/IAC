8de3c658ca6fac79946813e9bdb63d38a0d79918
Ruben <s1190828@student.windesheim.nl>
2025-05-18 07:03:09 +0000

    verwijziging naar location en subc.id veranderd

--- IAC/les02/2B/main.tf
+++ IAC/les02/2B/main.tf
@@ -12,22 +12,22 @@ provider "azurerm" {
   subscription_id = "7c2cf771-1067-4e56-9047-4a218905ddaf"  
 }
 
-#verwijzing voor de resourcegroup hier zodat deze niet steeds ingevuld hoeft te worden
-resource "azurerm_resource_group" "rg" {
-  name     = "s1190828"
-  location = "westeurope"
+#resource group als data importeren zodat deze wel gebruikt kan worden, maar niet beheerd wordt door Terraform
+data "azurerm_resource_group" "rg" {
+  name = "s1190828"
 }
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "2bvnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "2bsubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -35,8 +35,8 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_public_ip" "pip" {
   count               = 2
   name                = "2b-publicip-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Basic"
 }
@@ -44,8 +44,8 @@ resource "azurerm_public_ip" "pip" {
 resource "azurerm_network_interface" "nic" {
   count               = 2
   name                = "iac-nic-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -58,8 +58,8 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_linux_virtual_machine" "vm" {
   count                              = 2
   name                               = "iac-vm-${count.index}"
-  resource_group_name                = azurerm_resource_group.rg.name
-  location                           = azurerm_resource_group.rg.location
+  resource_group_name                = data.azurerm_resource_group.rg.name
+  location                           = data.azurerm_resource_group.rg.location
   size                               = "Standard_B2ats_v2"
   admin_username                     = "iac"
   disable_password_authentication    = true
